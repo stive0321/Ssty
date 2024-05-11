7793fcb3bf8b4a33ee49ddf8f6b232c9881b6e3c
stive0321 <165804330+stive0321@users.noreply.github.com>
2024-05-11 18:22:31 +0100

    Update help.js

--- scripts/cmds/help.js
+++ scripts/cmds/help.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const path = require("path");
 const { getPrefix } = global.utils;
 const { commands, aliases } = global.GoatBot;
-const doNotDelete = "[ 🧜‍♂🌪 | ❤‍🔥𝚃𝚒𝚝𝚞𝚜☠️]";
+const doNotDelete = "[😶🚬|Stanley stawa ]";
 /** 
 * @author NTKhang
 * @author: do not delete it
